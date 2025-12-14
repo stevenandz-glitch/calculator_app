a7fd07a646dce7598d8d479a8852a65de651b06c
stevenandz-glitch <stevenandz92491@gmail.com>
2025-12-14 09:51:45 -0800

    Added service worker and created server

---
+++ service_worker.js
@@ -0,0 +1,24 @@
+const cache_name = "calculator-v1"
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/sounds/",
+  "/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cache_name).then (cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
